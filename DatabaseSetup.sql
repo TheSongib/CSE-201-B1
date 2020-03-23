9116ac8ee36cf5d9cd85cf33b8ad615cd87dfd58
Brandon Gibson <bcg600@gmail.com>
2020-03-23 02:24:42 -0400

    Adding Instructions to setup project and updated database script

---
+++ DatabaseSetup.sql
@@ -0,0 +1,49 @@
+﻿USE master
+GO
+
+IF DB_ID('Store') IS NOT NULL	DROP DATABASE Store
+GO
+
+CREATE DATABASE Store
+GO
+
+USE [Store]
+GO
+
+CREATE TABLE [dbo].[tblUsers] (
+		userId		INT				NOT NULL	PRIMARY KEY		IDENTITY,
+		email		varchar(100)	NOT NULL,
+		[password]	varchar(100)	NOT NULL,
+		[address]	varchar(50)		NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[tblListings] (
+		listingId		INT					NOT NULL	 PRIMARY KEY		IDENTITY,
+		userId			INT					NOT NULL,
+		title			varchar(100)		NOT NULL,
+		decscription	varchar(1000)
+)
+GO
+
+CREATE TABLE [dbo].[tblImages] (
+		imageId		INT					NOT NULL	 PRIMARY KEY		IDENTITY,
+		listingId	INT					NOT NULL,
+		imageName	VARCHAR(50)			NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[tblCart] (
+		cartId		INT					NOT NULL	 PRIMARY KEY		IDENTITY,
+		userId		INT					NOT NULL,
+		listingId	INT					NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[tblWishList] (
+		wishListId		INT					NOT NULL	 PRIMARY KEY		IDENTITY,
+		userId			INT					NOT NULL,
+		listingId		INT					NOT NULL
+)
+GO
+
--- OnlineClothingStore/Sell.aspx
+++ OnlineClothingStore/Sell.aspx
@@ -14,13 +14,19 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-lg">
+            <div class="form-group">
+                <label for="title">Listing Title: </label>
+                <input type="text" class="form-control" id="title" name="title">
+                <label for="description">Description</label><br />
+                <textarea style="vertical-align: middle;" id="description" name="description" rows="5" cols="40">"Put description here."</textarea>
+            </div>
+        </div>
+    </div>
 
     <div class="form-group">
-        <label for="title">Listing Title: </label>
-        <input type="text" class="form-control" id="title" name="title">
-        <label for="description">Description</label><br />
-        <textarea style="vertical-align: middle;" id="description" name="description" rows="5" cols="40">"Put description here."</textarea>
-
+        <input type="submit" id="submit" name="submit">
     </div>
 
 </asp:Content>
