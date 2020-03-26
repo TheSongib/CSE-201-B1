9dec3d8ab5092875c6ce3fd243dc51d356a0e347
Brandon Gibson <bcg600@gmail.com>
2020-03-26 14:35:39 -0400

    Updated Instructions and SQL server

--- DatabaseSetup.sql
+++ DatabaseSetup.sql
@@ -12,16 +12,21 @@ GO
 
 CREATE TABLE [dbo].[tblUsers] (
 		userId		INT				NOT NULL	PRIMARY KEY		IDENTITY,
+		firstName	varchar(100)	NOT NULL,
+		lastName	varchar(100)	NOT NULL,
 		email		varchar(100)	NOT NULL,
-		[password]	varchar(100)	NOT NULL,
-		[address]	varchar(50)		NOT NULL
+		[password]	nvarchar(100)	NOT NULL,
+		[address]	varchar(100)	NOT NULL,
+		rating		DECIMAL(2,2)
 )
 GO
 
 CREATE TABLE [dbo].[tblListings] (
-		listingId		INT					NOT NULL	 PRIMARY KEY		IDENTITY,
+		listingId		INT					NOT NULL	PRIMARY KEY		IDENTITY,
 		userId			INT					NOT NULL,
 		title			varchar(100)		NOT NULL,
+		price			INT					NOT NULL	DEFAULT(0),
+		category		varchar(50)			NOT NULL,
 		decscription	varchar(1000)
 )
 GO
@@ -47,3 +52,84 @@ CREATE TABLE [dbo].[tblWishList] (
 )
 GO
 
+CREATE PROCEDURE spValidateLogin
+	@email   varchar(100),
+	@password   nvarchar(100),
+	@success INT OUTPUT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblUsers WHERE (email = @email) AND (password = @password)) BEGIN
+		SET @success = (SELECT userId FROM tblUsers WHERE (email = @email) AND (password = @password))
+	END ELSE BEGIN
+		SET @success = 0 
+	END
+END
+GO
+
+CREATE PROCEDURE spCreateAccount
+	@firstName   varchar(100),
+	@lastName   varchar(100),
+	@email   varchar(100),
+	@password   nvarchar(100),
+	@address varchar(100),
+	@success BIT OUTPUT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblUsers WHERE (email = @email)) BEGIN
+		SET @success = 0
+	END ELSE BEGIN
+		INSERT INTO tblUsers(firstName, lastName, email, [password], [address]) VALUES (@firstName, @lastName, @email, @password, @address)
+		SET @success = 1 
+	END
+END
+GO
+
+CREATE PROCEDURE spGetCart
+	@userId INT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblCart WHERE (userId = @userId)) BEGIN
+		SELECT -1
+	END ELSE BEGIN
+		SELECT l.* FROM listings l, cart c WHERE c.userId = @userId AND l.listingId = c.listingId
+	END
+END
+GO
+
+CREATE PROCEDURE spAddRemoveCart
+	@userId INT,
+	@listingId INT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblCart WHERE (listingId = @listingId)) BEGIN
+		DELETE FROM tblCart WHERE listingId = @listingId
+	END ELSE BEGIN
+		INSERT INTO tblCart(listingId, userId) VALUES (@listingId, @userId)
+	END
+END
+GO
+
+CREATE PROCEDURE spGetWishlist
+	@userId INT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblWishList WHERE (userId = @userId)) BEGIN
+		SELECT -1
+	END ELSE BEGIN
+		SELECT l.* FROM listings l, tblWishList w WHERE w.userId = @userId AND l.listingId = w.listingId
+	END
+END
+GO
+
+CREATE PROCEDURE spAddRemoveWishlist
+	@userId INT,
+	@listingId INT
+AS    BEGIN
+	IF EXISTS(SELECT NULL FROM tblWishList WHERE (listingId = @listingId)) BEGIN
+		DELETE FROM tblWishList WHERE listingId = @listingId
+	END ELSE BEGIN
+		INSERT INTO tblWishList(listingId, userId) VALUES (@listingId, @userId)
+	END
+END
+GO
+
+CREATE PROCEDURE spGetListings
+AS    BEGIN
+	SELECT * FROM tblListings
+END
+GO
--- OnlineClothingStore/Sell.aspx.cs
+++ OnlineClothingStore/Sell.aspx.cs
@@ -1,17 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Serialization;
 namespace OnlineClothingStore
 {
+    
+
     public partial class Contact : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+    }
+
+    public DataSet getDelivery()
+    {
+        SqlConnection conn = new SqlConnection();
+        StringBuilder sql = new StringBuilder();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet deliveryData = new Dataset();
+
+        conn = dbConn.getConnection();
+        deliveryData = new DataSet();
+        sql = new StringBuilder();
+        sql.AppendLine("SELECT * FROM DeliveryRec");
+
+        conn.Open();
+        da = new SqlDataAdapter(sql.ToString(), conn);
+        da.Fill(deliveryData);
+
+        return deliveryData;
     }
 }
